6fd4ee3ba2f283728495432b662f48be03298fe6
Larry Cherry <larry.cherry.0406@gmail.com>
2019-01-15 15:19:59 -0600

    implemented static controls

---
+++ src/components/Burger/BuildControls/BuildControls.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import BuildControl from './BuildControl/BuildControl';
+import classes from './BuildControls.css';
+
+const controls =  [
+    {label: 'Salad', type: 'salad'},
+    {label: 'Bacon', type: 'bacon'},
+    {label: 'Cheese', type: 'cheese'},
+    {label: 'Meat', type: 'meat'}
+]
+
+const buildControls = (props) => (
+    <div className={classes.BuildControls} id=''>
+        {controls.map( control => {
+            return <BuildControl key={control.label} label={control.label}/>
+        })}
+    </div>
+);
+
+export default buildControls;
--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -3,13 +3,24 @@ import BurgerIngredient from './BurgerIngredient/BurgerIngredient';
 import classes from './Burger.css';
 
 const burger = (props) => {
+    let transformedIngredients = Object.keys(props.ingredients)
+        .map( igKey => {
+            return [...Array(props.ingredients[igKey])].map( (_, index) => {
+                return <BurgerIngredient key={igKey + index} type={igKey}/>
+            });
+        })
+        .reduce( (arr, el) => {
+            return arr.concat(el);
+        }, []);
+        console.log(transformedIngredients);
+        if(transformedIngredients.length === 0){
+            transformedIngredients = <p>Please Start Adding Ingredients</p>
+        }
     return (
         <div className={classes.Burger}>
             <BurgerIngredient type='bread-top'/>
-            <BurgerIngredient type='cheese'/>
-            <BurgerIngredient type='meat'/>
+                {transformedIngredients}
             <BurgerIngredient type='bread-bottom'/>
-            
         </div>
     );
 };
