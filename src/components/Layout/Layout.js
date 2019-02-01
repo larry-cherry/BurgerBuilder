cdd9047bfc2201858cbec60d04238ab7630da9d5
Larry Cherry <larry.cherry.0406@gmail.com>
2019-01-31 23:59:53 -0600

    added navigation

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import AuxComp from '../../hoc/AuxComp';
 import classes from './Layout.css';
+import Toolbar from '../../components/Navigation/Toolbar/Toolbar';
 
 const layout = (props) => (
     <AuxComp>
+        <Toolbar/>
         <div>Toolbar, Sidedrawer, Backdrop</div>
         <main className={classes.Content}>
             {props.children}
